f4493154b4714cb2f2e08f77c3b940fd6c8ac0d6
Lucas Lima Romero <101420277+luckera@users.noreply.github.com>
2023-06-08 13:29:34 -0300

    Pushbuchetti

--- Templates Robson/exercicio2c.c
+++ Templates Robson/exercicio2c.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <assert.h>
-#include <string.h>
+#include <string.h> // funções strcmp e strcpy
 #include <math.h>
 
 // Definição das variaveis que controlam a medição de tempo
@@ -13,11 +12,76 @@ typedef unsigned char bool;
 #define TRUE  1
 #define FALSE 0
 
+#define N 50000
+#define M 70000
+#define B 150001
+
 // Definição do tipo string
 typedef char * string;
 
 #define MAX_STRING_LEN 20
 
+unsigned colisoes_h_div = 0;
+unsigned colisoes_h_mul = 0;
+unsigned encontrados_h_div = 0;
+unsigned encontrados_h_mul = 0;
+
+// TAD Lista encadeada
+
+typedef struct node{
+    long key;
+    struct node *next; // Não podemos colocar Node *next, pois o compilador lê de cima para baixo, e ainda não sabe o que é Node
+} No;
+
+typedef struct{
+    No *begin; // Ponteiro para o primeiro nó
+    No *end; // Ponteiro para o último nó, utilizado na hora de adicionar na cauda da lista
+    long size;
+} Lista;
+
+Lista *cria_Lista(){
+    Lista *L = (Lista *) malloc(sizeof(Lista)); 
+    L->begin = L->end = NULL;
+    L->size = 0;
+
+    return L;
+}
+
+void destroi_Lista(Lista **L_ref){
+    Lista *L = *L_ref;
+
+    No *p = L->begin;
+    No *aux = NULL; // Preciso do aux, pois ao dar free(p) eu perderia o p->next
+
+    while(p != NULL){
+        aux = p; // aux aponta para onde o p aponta (salvamos a posição)
+        p = p->next; // avançamos uma posição
+        free(aux);
+    }
+    free(L); // Desalocamos a lista em si
+
+    *L_ref = NULL;
+}
+
+No *cria_No(long key){
+    No *no = (No *) malloc(sizeof(No));
+    no->key = key;
+    no->next = NULL;
+
+    return no;
+}
+
+int insere_No(Lista *L, long key){
+    No *no = cria_No(key);
+
+    if(L->size == 0)
+        L->begin = L->end = no;
+    else{
+        L->end->next = no;
+        L->end = no;
+    }
+    L->size++;
+}
 
 unsigned converter(string s) {
    unsigned h = 0;
@@ -54,60 +118,71 @@ double finaliza_tempo()
     return ((double) (_fim - _ini)) / CLOCKS_PER_SEC;
 }
 
-unsigned h_div(unsigned x, unsigned B)
+unsigned h_div(unsigned x)
 {
-    return x % B;
+    return (x % B);
 }
 
-unsigned h_mul(unsigned x, unsigned B)
+unsigned h_mul(unsigned x)
 {
     const double A = 0.6180;
     return fmod(x * A, 1) * B;
 }
 
+typedef struct{
+    Lista *vet[B];
+}hash;
+
+void inicializar(hash *tabela){
+    for(int i = 0; i < B; i++)
+        tabela->vet[i] = cria_Lista();
+}
+
+long inserir_div(hash *tabela, long k){
+    long pos = h_div(k);
+    return (insere_No(tabela->vet[pos], k) == 0) ? pos : -1;
+}
+
+
 int main(int argc, char const *argv[])
 {
-    const int N = 50000;
-    const int M = 70000;
-    const int B = 150001;
-
-    unsigned colisoes_h_div = 0;
-    unsigned colisoes_h_mul = 0;
-
-    unsigned encontrados_h_div = 0;
-    unsigned encontrados_h_mul = 0;
-
     string* insercoes = ler_strings("strings_entrada.txt", N);
     string* consultas = ler_strings("strings_busca.txt", M);
-    
+
 
     // cria tabela hash com hash por divisão
-    
-    // inserção dos dados na tabela hash com hash por divisão
+    long tabela_div[B];
+    inicializar(tabela_div);
+
+    // inserção dos dados na tabela hash usando hash por divisão
     inicia_tempo();
     for (int i = 0; i < N; i++) {
         // inserir insercoes[i] na tabela hash
+        inserir_div(tabela_div, converter(insercoes[i]));
     }
     double tempo_insercao_h_div = finaliza_tempo();
 
-    // busca dos dados na tabela hash com hash por divisão
+    // consulta dos dados na tabela hash usando hash por divisão
     inicia_tempo();
     for (int i = 0; i < M; i++) {
         // buscar consultas[i] na tabela hash
+        buscar_div(tabela_div, converter(consultas[i]));
     }
     double tempo_busca_h_div = finaliza_tempo();
 
-    // destroi tabela hash com hash por divisão
-
-
+    // limpa a tabela hash com hash por divisão
+    inicializar(tabela_div);
 
 
     // cria tabela hash com hash por multiplicação
+    long tabela_mul[B];
+    inicializar(tabela_mul);
 
-    // inserção dos dados na tabela hash com hash por multiplicação
+    // inserção dos dados na tabela hash usando hash por multiplicação
     inicia_tempo();
     for (int i = 0; i < N; i++) {
         // inserir insercoes[i] na tabela hash
+        inserir_mul(tabela_mul, converter(insercoes[i]));
     }
     double tempo_insercao_h_mul = finaliza_tempo();
 
@@ -115,24 +190,34 @@ int main(int argc, char const *argv[])
     inicia_tempo();
     for (int i = 0; i < M; i++) {
         // buscar consultas[i] na tabela hash
+        buscar_mul(tabela_mul, converter(consultas[i]));
     }
     double tempo_busca_h_mul = finaliza_tempo();
 
-    // destroi tabela hash com hash por multiplicação
+    // limpa a tabela hash com hash por multiplicação
+    inicializar(tabela_mul);
 
-
-
-    printf("Hash por Divisão\n");
-    printf("Colisões na inserção: %d\n", colisoes_h_div);
-    printf("Tempo de inserção   : %fs\n", tempo_insercao_h_div);
+    printf("Hash por Divisao\n");
+    printf("Colisoes na insercao: %d\n", colisoes_h_div);
+    printf("Tempo de insercao   : %fs\n", tempo_insercao_h_div);
     printf("Tempo de busca      : %fs\n", tempo_busca_h_div);
     printf("Itens encontrados   : %d\n", encontrados_h_div);
     printf("\n");
-    printf("Hash por Multiplicação\n");
-    printf("Colisões na inserção: %d\n", colisoes_h_mul);
-    printf("Tempo de inserção   : %fs\n", tempo_insercao_h_mul);
+    printf("Hash por Multiplicacao\n");
+    printf("Colisoes na insercao: %d\n", colisoes_h_mul);
+    printf("Tempo de insercao   : %fs\n", tempo_insercao_h_mul);
     printf("Tempo de busca      : %fs\n", tempo_busca_h_mul);
     printf("Itens encontrados   : %d\n", encontrados_h_mul);
+
+    for(int i = 0; i < N; i++){
+        free(insercoes[i]);
+    }
+    free(insercoes);
+
+    for(int i = 0; i < M; i++){
+        free(consultas[i]);
+    }
+    free(consultas); 
 
     return 0;
 }
